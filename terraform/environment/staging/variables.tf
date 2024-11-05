778ab5dfa3db4aa6c6583d917597750505f02c4a
deepenc <sunilojha525@gmail.com>
2024-11-05 22:48:39 +0545

    feat: modify the terrafrom structure

---
+++ terraform/environment/staging/variables.tf
@@ -0,0 +1,18 @@
+variable "bucket_name" {
+    type = string
+    default = "terraform-state-file-dev-1.0"
+}
+
+variable "kubernetes_version" {
+  default     = 1.27
+  description = "kubernetes version"
+}
+
+variable "vpc_cidr" {
+  default     = "10.0.0.0/16"
+  description = "default CIDR range of the VPC"
+}
+variable "region" {
+  default = "us-east-1"
+  description = "aws region"
+}
